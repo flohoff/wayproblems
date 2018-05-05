24e9c544c548b1f672c9b4ea0e4cb7508505a40e
Florian Lohoff <f@zz.de>
2018-05-05 14:35:10 +0200

    Update sql inserts

---
+++ wayproblems-meta-local.sql
@@ -0,0 +1,14 @@
+INSERT INTO "meta" VALUES('layer.footway.shortdescription', 'footway issues for owl');
+INSERT INTO "meta" VALUES('layer.footway.boundary', ( readfile("owl.geojson") ));
+
+INSERT INTO "meta" VALUES('layer.ref.shortdescription', 'ref=* issues for owl');
+INSERT INTO "meta" VALUES('layer.ref.boundary', ( readfile("owl.geojson") ));
+
+INSERT INTO "meta" VALUES('layer.wayproblems.shortdescription', 'Way problems for owl');
+INSERT INTO "meta" VALUES('layer.wayproblems.boundary', ( readfile("owl.geojson") ));
+
+
+INSERT INTO "meta" VALUES ( 'time.data', '2018-05-03T12:35:02Z');
+INSERT INTO "meta" VALUES ( 'time.process', '2018-05-03T12:35:02Z');
+
+
--- wayproblems-meta.sql
+++ wayproblems-meta.sql
@@ -36,10 +36,66 @@ INSERT INTO "meta" VALUES('layer.wayproblems.columns:6','problem');
 INSERT INTO "meta" VALUES('layer.wayproblems.columns:7','style');
 INSERT INTO "meta" VALUES('layer.wayproblems.popup', ( readfile('wayproblems-meta.popup' )) );
 
-INSERT INTO "meta" VALUES('layer.wayproblems.shortdescription', 'Way problems for owl');
-INSERT INTO "meta" VALUES('layer.wayproblems.boundary', ( readfile("owl.geojson") ));
+INSERT INTO "meta" VALUES('layer.ref.geometrycolumn','geometry');
+INSERT INTO "meta" VALUES('layer.ref.srid','4326');
 
-INSERT INTO "meta" VALUES ( 'time.data', '2018-05-03T12:35:02Z');
-INSERT INTO "meta" VALUES ( 'time.process', '2018-05-03T12:35:02Z');
+INSERT INTO "meta" VALUES('layer.ref.styles.default.color','#ff0000');
+INSERT INTO "meta" VALUES('layer.ref.styles.default.weight','4');
+INSERT INTO "meta" VALUES('layer.ref.styles.default.opacity','0.9');
+
+INSERT INTO "meta" VALUES('layer.ref.styles.ref.color','#000080');
+INSERT INTO "meta" VALUES('layer.ref.styles.ref.weight','3');
+INSERT INTO "meta" VALUES('layer.ref.styles.ref.opacity','0.9');
+
+INSERT INTO "meta" VALUES('layer.ref.styles.footway.color','#707000');
+INSERT INTO "meta" VALUES('layer.ref.styles.footway.weight','3');
+INSERT INTO "meta" VALUES('layer.ref.styles.footway.opacity','0.9');
+
+INSERT INTO "meta" VALUES('layer.ref.styles.redundant.color','#007070');
+INSERT INTO "meta" VALUES('layer.ref.styles.redundant.weight','3');
+INSERT INTO "meta" VALUES('layer.ref.styles.redundant.opacity','0.6');
+
+INSERT INTO "meta" VALUES('layer.ref.stylecolumn','style');
+INSERT INTO "meta" VALUES('layer.ref.columns:0','id');
+INSERT INTO "meta" VALUES('layer.ref.columns:1','key');
+INSERT INTO "meta" VALUES('layer.ref.columns:2','value');
+INSERT INTO "meta" VALUES('layer.ref.columns:3','changeset');
+INSERT INTO "meta" VALUES('layer.ref.columns:4','user');
+INSERT INTO "meta" VALUES('layer.ref.columns:5','timestamp');
+INSERT INTO "meta" VALUES('layer.ref.columns:6','problem');
+INSERT INTO "meta" VALUES('layer.ref.columns:7','style');
+INSERT INTO "meta" VALUES('layer.ref.popup', ( readfile('wayproblems-meta.popup' )) );
+
+
+
+INSERT INTO "meta" VALUES('layer.footway.geometrycolumn','geometry');
+INSERT INTO "meta" VALUES('layer.footway.srid','4326');
+
+INSERT INTO "meta" VALUES('layer.footway.styles.default.color','#ff0000');
+INSERT INTO "meta" VALUES('layer.footway.styles.default.weight','4');
+INSERT INTO "meta" VALUES('layer.footway.styles.default.opacity','0.9');
+
+INSERT INTO "meta" VALUES('layer.footway.styles.ref.color','#000080');
+INSERT INTO "meta" VALUES('layer.footway.styles.ref.weight','3');
+INSERT INTO "meta" VALUES('layer.footway.styles.ref.opacity','0.9');
+
+INSERT INTO "meta" VALUES('layer.footway.styles.footway.color','#707000');
+INSERT INTO "meta" VALUES('layer.footway.styles.footway.weight','3');
+INSERT INTO "meta" VALUES('layer.footway.styles.footway.opacity','0.9');
+
+INSERT INTO "meta" VALUES('layer.footway.styles.redundant.color','#007070');
+INSERT INTO "meta" VALUES('layer.footway.styles.redundant.weight','3');
+INSERT INTO "meta" VALUES('layer.footway.styles.redundant.opacity','0.6');
+
+INSERT INTO "meta" VALUES('layer.footway.stylecolumn','style');
+INSERT INTO "meta" VALUES('layer.footway.columns:0','id');
+INSERT INTO "meta" VALUES('layer.footway.columns:1','key');
+INSERT INTO "meta" VALUES('layer.footway.columns:2','value');
+INSERT INTO "meta" VALUES('layer.footway.columns:3','changeset');
+INSERT INTO "meta" VALUES('layer.footway.columns:4','user');
+INSERT INTO "meta" VALUES('layer.footway.columns:5','timestamp');
+INSERT INTO "meta" VALUES('layer.footway.columns:6','problem');
+INSERT INTO "meta" VALUES('layer.footway.columns:7','style');
+INSERT INTO "meta" VALUES('layer.footway.popup', ( readfile('wayproblems-meta.popup' )) );
 
 COMMIT;
