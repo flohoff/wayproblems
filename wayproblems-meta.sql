047f613659b5367822a05da1a7bef135d9256b13
Florian Lohoff <f@zz.de>
2018-05-14 13:40:04 +0200

    Merge strange and defaults layer

--- wayproblems-meta.sql
+++ wayproblems-meta.sql
@@ -98,6 +98,68 @@ INSERT INTO "meta" VALUES('layer.footway.columns:6','problem');
 INSERT INTO "meta" VALUES('layer.footway.columns:7','style');
 INSERT INTO "meta" VALUES('layer.footway.popup', ( readfile('wayproblems-meta.popup' )) );
 
+INSERT INTO "meta" VALUES('layer.track.geometrycolumn','geometry');
+INSERT INTO "meta" VALUES('layer.track.srid','4326');
+
+INSERT INTO "meta" VALUES('layer.track.styles.default.color','#ff0000');
+INSERT INTO "meta" VALUES('layer.track.styles.default.weight','4');
+INSERT INTO "meta" VALUES('layer.track.styles.default.opacity','0.9');
+
+INSERT INTO "meta" VALUES('layer.track.styles.ref.color','#000080');
+INSERT INTO "meta" VALUES('layer.track.styles.ref.weight','3');
+INSERT INTO "meta" VALUES('layer.track.styles.ref.opacity','0.9');
+
+INSERT INTO "meta" VALUES('layer.track.styles.footway.color','#707000');
+INSERT INTO "meta" VALUES('layer.track.styles.footway.weight','3');
+INSERT INTO "meta" VALUES('layer.track.styles.footway.opacity','0.9');
+
+INSERT INTO "meta" VALUES('layer.track.styles.redundant.color','#007070');
+INSERT INTO "meta" VALUES('layer.track.styles.redundant.weight','3');
+INSERT INTO "meta" VALUES('layer.track.styles.redundant.opacity','0.6');
+
+INSERT INTO "meta" VALUES('layer.track.stylecolumn','style');
+INSERT INTO "meta" VALUES('layer.track.columns:0','id');
+INSERT INTO "meta" VALUES('layer.track.columns:1','key');
+INSERT INTO "meta" VALUES('layer.track.columns:2','value');
+INSERT INTO "meta" VALUES('layer.track.columns:3','changeset');
+INSERT INTO "meta" VALUES('layer.track.columns:4','user');
+INSERT INTO "meta" VALUES('layer.track.columns:5','timestamp');
+INSERT INTO "meta" VALUES('layer.track.columns:6','problem');
+INSERT INTO "meta" VALUES('layer.track.columns:7','style');
+INSERT INTO "meta" VALUES('layer.track.popup', ( readfile('wayproblems-meta.popup' )) );
+
+INSERT INTO "meta" VALUES('layer.strange.geometrycolumn','geometry');
+INSERT INTO "meta" VALUES('layer.strange.srid','4326');
+
+INSERT INTO "meta" VALUES('layer.strange.styles.default.color','#ff0000');
+INSERT INTO "meta" VALUES('layer.strange.styles.default.weight','4');
+INSERT INTO "meta" VALUES('layer.strange.styles.default.opacity','0.9');
+
+INSERT INTO "meta" VALUES('layer.strange.styles.ref.color','#000080');
+INSERT INTO "meta" VALUES('layer.strange.styles.ref.weight','3');
+INSERT INTO "meta" VALUES('layer.strange.styles.ref.opacity','0.9');
+
+INSERT INTO "meta" VALUES('layer.strange.styles.footway.color','#707000');
+INSERT INTO "meta" VALUES('layer.strange.styles.footway.weight','3');
+INSERT INTO "meta" VALUES('layer.strange.styles.footway.opacity','0.9');
+
+INSERT INTO "meta" VALUES('layer.strange.styles.redundant.color','#007070');
+INSERT INTO "meta" VALUES('layer.strange.styles.redundant.weight','3');
+INSERT INTO "meta" VALUES('layer.strange.styles.redundant.opacity','0.6');
+
+INSERT INTO "meta" VALUES('layer.strange.stylecolumn','style');
+INSERT INTO "meta" VALUES('layer.strange.columns:0','id');
+INSERT INTO "meta" VALUES('layer.strange.columns:1','key');
+INSERT INTO "meta" VALUES('layer.strange.columns:2','value');
+INSERT INTO "meta" VALUES('layer.strange.columns:3','changeset');
+INSERT INTO "meta" VALUES('layer.strange.columns:4','user');
+INSERT INTO "meta" VALUES('layer.strange.columns:5','timestamp');
+INSERT INTO "meta" VALUES('layer.strange.columns:6','problem');
+INSERT INTO "meta" VALUES('layer.strange.columns:7','style');
+INSERT INTO "meta" VALUES('layer.strange.popup', ( readfile('wayproblems-meta.popup' )) );
+
+
+
 INSERT INTO "meta" VALUES('layer.defaults.geometrycolumn','geometry');
 INSERT INTO "meta" VALUES('layer.defaults.srid','4326');
 
@@ -127,5 +189,7 @@ INSERT INTO "meta" VALUES('layer.defaults.columns:5','timestamp');
 INSERT INTO "meta" VALUES('layer.defaults.columns:6','problem');
 INSERT INTO "meta" VALUES('layer.defaults.columns:7','style');
 INSERT INTO "meta" VALUES('layer.defaults.popup', ( readfile('wayproblems-meta.popup' )) );
+
+
 
 COMMIT;
